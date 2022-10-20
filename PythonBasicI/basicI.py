6a6cb014502cd3c9b781ecc554a3fcb84f449ebb
vicente_ogalde <vicenteogalde@gmail.com>
2022-10-20 16:07:44 -0300

    1st

---
+++ PythonBasicI/basicI.py
@@ -0,0 +1,8 @@
+#Variables
+number=5 #is like javascript the conten give you the type
+print(type(number))#in python everything is an object , is 100% OOP language
+phrase="hello world" # unlike JAVA when you specific the type example : int number=1;
+print(number,phrase)
+
+
+
