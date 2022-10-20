5eeb4166c30df8f3a210f69e52e67f24fc526231
vicente_ogalde <vicenteogalde@gmail.com>
2022-10-20 20:26:28 -0300

    basic complete

--- PythonBasicI/basicI.py
+++ PythonBasicI/basicI.py
@@ -1,8 +1,18 @@
 #Variables
-number=5 #is like javascript the conten give you the type
+number=5 #is like javascript the content give you the type
 print(type(number))#in python everything is an object , is 100% OOP language
 phrase="hello world" # unlike JAVA when you specific the type example : int number=1;
 print(number,phrase)
+number2=7 # = is an assignment operator that gives a value to the variable
+if number>number2: # > is a comparison operator
+#if the comparison is true the following line of code is executed 
+    print("number is greater than number2")
+else:
+# if not automatically the following block of code is executed
+    print("number2 is greater than number")
+
+
+
 
 
 
