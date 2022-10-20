6154b0a2906ac54daf3e75ebb3d075867f43832d
vicente_ogalde <vicenteogalde@gmail.com>
2022-10-20 20:58:27 -0300

    functions update

---
+++ PythonBasicI/basicIIFunctions.py
@@ -0,0 +1,12 @@
+#print is a predefine function
+#message is not a predefined function, it is made by the programmer
+#def is a word reserved by the language python to declare a function, that is a reusable code block
+#you can call a function by typing its name, you can call it as many times as necessary
+def message():
+    print("we are learning python")
+
+message()
+message()
+print("code out of the function message")
+message()
+
