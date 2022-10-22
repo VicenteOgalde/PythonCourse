4711bd39c26283bbacbe3e5f31679cf68cbd3240
vicente_ogalde <vicenteogalde@gmail.com>
2022-10-22 20:12:58 -0300

    updated dictionaries

--- PythonBasicI/basicVDictionaries.py
+++ PythonBasicI/basicVDictionaries.py
@@ -12,6 +12,9 @@ meList=["spain","france"]
 meDictionary2={meList[0]:"madrid",meList[1]:"paris"} #adding some keys to meList
 print(meDictionary2)
 print(meDictionary2["spain"]) #print with the key on the list
+print(len(meDictionary2)) #print the length
+print(meDictionary2.keys()) #print only the keys
+print(meDictionary2.values()) #print only the values
 
 
 
