59e7b40a803fee0da64075c4603daf3dccae43ec
vicente_ogalde <vicenteogalde@gmail.com>
2022-10-24 20:20:32 -0300

    added practice FOR

---
+++ PythonLoops/1.practiceFOR.py
@@ -0,0 +1,8 @@
+# Loops are used to repeat a statement or block of code
+# Loops can be determinate or indeterminate
+# The difference being that with a determinate loop structure, one can (normally) predict exactly how many times the loop will repeat; where as with an indeterminate loop structure, this is not always the case. An indeterminate loop structure loops as long as a condition evaluates to some certain boolean value.
+
+for i in [1,2,3]: #for i in [] loop through the data structure list,tuple,etc
+    print("hello")
+    print(i)
+
