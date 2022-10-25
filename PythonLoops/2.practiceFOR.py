28f6cd0ba0cc9bd33fc823347a43a7c0ca9d9524
vicente_ogalde <vicenteogalde@gmail.com>
2022-10-25 20:18:06 -0300

    added for in range

--- PythonLoops/2.practiceFOR.py
+++ PythonLoops/2.practiceFOR.py
@@ -2,12 +2,10 @@ email=False
 flag=0
 in_email=input("Type your email")
 for i in in_email: #loop through the string checking all the character
-    if i=="@" or i==".": #check if one character is @
+    if i=="@" or i==".": #check if one character is @ or .
         flag=flag+1
 if flag>=2: # check if the value is true
     print("correct email")
 else:
     print("incorrect email")
 
-for x in range(5): #repeats the number of times that is entered in the range
-    print("using range in for "+str(x+1))
---
+++ PythonLoops/3.practiceFOR.py
@@ -0,0 +1,4 @@
+for i in range(5): #repeats the number of times that is entered in the range
+    print(f"using range in for {i}")
+for i in range(5,50,5):
+    print(f"using range jumping from 5 to 5 until number 50 :{i}")
