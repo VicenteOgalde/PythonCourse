aa7fb732d70b65205343e4b4dd63ffbb3bf07fe9
vicente_ogalde <vicenteogalde@gmail.com>
2022-10-30 20:38:52 -0300

    added info about encapsulate

--- PythonOOP/1.practiceOOP.py
+++ PythonOOP/1.practiceOOP.py
@@ -2,25 +2,31 @@ from operator import truediv
 
 
 class Car(): #with the reserved word class you create your class
-    chassisLength=250 #this is an attribute of your class
-    chassisWidth=120
-    wheels=4
-    running=False
+    def __init__(self): #this is the constructor of your class, it contains the parameters with which the object of this will be created using the reserved word __init__
+        self.__chassisLength=250 #this is an attribute of your class
+        self.__chassisWidth=120 #using __attributeName you can encapsulate this attribute, to make it inaccessible from outside the class
+        self.__wheels=4
+        self.__running=False
 
-    def startCar(self):#A function becomes a method of the class if it is declared inside the class.
-        self.running=True
-    def state(self):
-        if(self.startCar):
+    def startCar(self, starting):#A function becomes a method of the class if it is declared inside the class.
+        self.__running=starting
+        if(self.__running):
             return "The car is running"
         else:
             return "The car is off"
+    def state(self):
+        print("the car have "+str(self.__wheels)+" wheels , chassis length of "+str(self.__chassisLength)+" and chassis width of "+str(self.__chassisWidth)+".")
 
 meCar= Car()#to instantiate a class or create an object just use this type of code nameObject=nameClass()
 
-print(meCar.chassisLength)#by using a dot after the object name you can access its attributes and methods
-print(meCar.running)
-meCar.startCar()
-print(meCar.state())
+
+print(meCar.startCar(True))#by using a dot after the object name you can access its attributes and methods
+meCar.state()
+
+print("---------------------")
+
+meCar2=Car()
+meCar2.state()
 
 
 
