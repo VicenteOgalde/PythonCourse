4ce6bda9c12c319c0c1330445ed5c3780b63c351
vicente_ogalde <vicenteogalde@gmail.com>
2022-10-31 14:23:10 -0300

    added content OOP

--- PythonOOP/1.practiceOOP.py
+++ PythonOOP/1.practiceOOP.py
@@ -1,6 +1,3 @@
-from operator import truediv
-
-
 class Car(): #with the reserved word class you create your class
     def __init__(self): #this is the constructor of your class, it contains the parameters with which the object of this will be created using the reserved word __init__
         self.__chassisLength=250 #this is an attribute of your class
@@ -11,11 +8,24 @@ class Car(): #with the reserved word class you create your class
     def startCar(self, starting):#A function becomes a method of the class if it is declared inside the class.
         self.__running=starting
         if(self.__running):
+            check=self.__internalChecking()
+        if(self.__running and check):
             return "The car is running"
+        elif(self.__running and check==False):
+            return "something wrong with the check"
         else:
             return "The car is off"
     def state(self):
         print("the car have "+str(self.__wheels)+" wheels , chassis length of "+str(self.__chassisLength)+" and chassis width of "+str(self.__chassisWidth)+".")
+    def __internalChecking(self):#using __functionName you can encapsulate this method, to make it inaccessible from outside the class
+        print("checking...")
+        self.gas="ok"
+        self.oil="ok"
+        self.doors="closed"
+        if self.gas=="ok" and self.oil=="ok" and self.doors=="closed":
+            return True
+        else:
+            return False
 
 meCar= Car()#to instantiate a class or create an object just use this type of code nameObject=nameClass()
 
