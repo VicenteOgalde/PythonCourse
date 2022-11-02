3cb540b1a1cfd80261d1d9f261ec9dc7cadb397c
vicente_ogalde <vicenteogalde@gmail.com>
2022-11-01 22:29:50 -0300

    added content

--- PythonOOP/2.practiceInheritance.py
+++ PythonOOP/2.practiceInheritance.py
@@ -13,9 +13,43 @@ class Vehicles():
         self.brake=True
     def state(self):
         print("Mark: ",self.mark,"\nModel: ",self.model,"\nRunning: ",self.running,"\nAccelerate: ",self.accelerate,"\nBrake: ",self.brake)
-class Motorcycle(Vehicles):#by putting the name of the class in parentheses when creating a new class it inherits all the attributes and methods
-    pass
+class Motorcycle(Vehicles):#by putting the name of the class in parentheses when creating a new class it inherits all the attributes and methods 
+    wheelie=""
+    def makeWheelie(self):#you can add new attributes and methods to the new subclass
+        self.wheelie="Doing a Wheelie"
+    def state(self):# you can overwrite the methods to adjust them to your new class
+        print("Mark: ",self.mark,"\nModel: ",self.model,"\nRunning: ",self.running,"\nAccelerate: ",self.accelerate,"\nBrake: ",self.brake,"\nWheelie: ",self.wheelie)
+
+class Van(Vehicles):
+    
+    def load(self, load):
+        self.loaded=load
+        if(self.loaded):
+            return "the van is loaded"
+        else:
+            return "the van is not loaded"
+
+class ElectricVehicle():
+    def __init__(self):
+        self.autonomy=100
+    def chargeEnergy(self):
+        self.charging=True
+    
+
 
 meMotorcycle=Motorcycle("Honda","CBR")
+meMotorcycle.makeWheelie()
 meMotorcycle.state()
+
+meVan=Van("ford","ab28")
+
+meVan.starUp()
+meVan.state()
+print(meVan.load(True))
+
+class ElectricBike(ElectricVehicle,Vehicles):#multiple inheritance , use the constructor of the first class introduced
+    pass
+
+meBike=ElectricBike()
+
 
