775a41b6655efdc83f7b01fc11dc6e1fe7a44f82
vicente_ogalde <vicenteogalde@gmail.com>
2022-11-06 13:32:52 -0300

    added a distributable package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="calculationPackage",
+    version="1.0",
+    description="Package of basic calculations",
+    author="Vicente Ogalde",
+    packages=["PythonPackages","PythonPackages.Calculations"]
+)
